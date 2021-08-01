00ef58f4355172aea1680b16b02f52938422492d
Svetoslav Tomov <svephoto@icloud.com>
2021-08-01 16:11:42 +0300

    New updates.

--- src/main/java/com/example/restfulwebservice/GreetingController.java
+++ src/main/java/com/example/restfulwebservice/GreetingController.java
@@ -10,6 +10,8 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicInteger counter = new AtomicInteger();
     private final GreetingRepository greetingRepository;
+    private final UserRepository userRepository;
+    private final MovieRepository movieRepository;
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
@@ -17,11 +19,14 @@ public class GreetingController {
     }
 
     @Autowired
-    public GreetingController(GreetingRepository greetingRepository) {
+    public GreetingController(GreetingRepository greetingRepository, UserRepository userRepository,
+                              MovieRepository movieRepository) {
         this.greetingRepository = greetingRepository;
+        this.userRepository = userRepository;
+        this.movieRepository = movieRepository;
     }
 
-    @GetMapping(path="/add")
+    @GetMapping(path = "/addGreeting")
     public Greeting addNewGreeting(@RequestParam String content) {
         Greeting currentGreeting = new Greeting();
 
@@ -30,8 +35,37 @@ public class GreetingController {
         return greetingRepository.save(currentGreeting);
     }
 
-    @GetMapping(path="/all")
-    public @ResponseBody Iterable<Greeting> getAllUsers() {
+    @GetMapping(path = "/allGreetings")
+    public @ResponseBody Iterable<Greeting> getAllGreetings() {
         return greetingRepository.findAll();
     }
+
+    @GetMapping(path = "/addMovie")
+    public Movie addNewMovie(@RequestParam Long id) {
+        Movie currentMovie = new Movie();
+
+        currentMovie.setId(id);
+
+        return movieRepository.save(currentMovie);
+    }
+
+    @GetMapping(path = "/allMovies")
+    public @ResponseBody Iterable<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+    @GetMapping(path = "/addUsers")
+    public User addNewUser(@RequestParam Long id) {
+        User currentUser = new User();
+
+        currentUser.setId(id);
+
+        return userRepository.save(currentUser);
+    }
+
+    @GetMapping(path = "/allUsers")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
 }
